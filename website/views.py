aecb9bec887816b178e5cf7941d62349f7114aad
Marquis Pierre <marquispierre27@gmail.com>
2025-10-15 19:42:01 -0400

    added registration

--- website/views.py
+++ website/views.py
@@ -20,16 +20,30 @@ def home(request):
     else:   
      return render(request, 'home.html', {})
 
-def login_out(request):
-    pass
-
-
-
-
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You Have Been Logged Out...")
+	return redirect('home')
 
 
 def register_user(request):
-    pass
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
 
 def customer_record(request, pk):
     pass
